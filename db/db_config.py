01430229790f7caa8f593fc0f2cc63f51ac0345a
ddanieldma <danielalmemiranda@gmail.com>
2025-06-17 12:10:07 -0300

    small fix to db_config fallback"

--- db/db_config.py
+++ db/db_config.py
@@ -12,7 +12,8 @@ DB_CONFIG = json.loads(dotenv_dbconfig) if dotenv_dbconfig else {
     'host': 'localhost',
     'user': 'postgres',
     'password': '123',
-    'port': '5432'
+    'port': '5432',
+    'database': 'ecommerce_db'
 } # fallback to default configuration
 
 DB_NAME = DB_CONFIG['database'] if dotenv_dbconfig else 'ecommerce_db'
