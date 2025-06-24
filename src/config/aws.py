67be247c4d308552e625edb87361ece6402de274
Luís Felipe Marciano <lfamarciano@gmail.com>
2025-06-24 20:59:41 -0300

    corrigi push errado kkk

--- src/config/aws.py
+++ src/config/aws.py
@@ -1,4 +1,12 @@
 # Arquivo para definir os endereços para os serviços na AWS.
 KAFKA_BROKER_URL = 'b-1.seu-cluster-msk...' # Endpoint do Amazon MSK
 REDIS_HOST = 'seu-cluster-redis.xxxx...'  # Endpoint do ElastiCache
+DB_CONFIG = {
+    'host': 'postgres.chvwsyfmunoi.us-east-1.rds.amazonaws.com', # <== COLE O ENDPOINT AQUI
+    'user': 'postgres', # O utilizador que você definiu ao criar o RDS
+    'password': '123', # A senha que você definiu
+    'port': '5432',
+    'database': 'ecommerce_db'
+}
+DB_NAME = 'ecommerce_db'
 # ... etc.
