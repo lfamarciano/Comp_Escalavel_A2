f59177ef91b8fd5e279431b559db9c5b6793e80a
ddanieldma <danielalmemiranda@gmail.com>
2025-06-26 00:46:15 -0300

    adds step of cleaning and starting fresh deltalake base before incremental bronze phase

---
+++ src/historical_pipeline/historical_load.py
@@ -0,0 +1,51 @@
+# historical_pipeline/historical_load.py
+import os
+from pyspark.sql import SparkSession
+
+from dotenv import load_dotenv
+load_dotenv()
+
+POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD", "123")
+POSTGRES_DATABASE = os.environ.get("POSTGRES_DATABASE", "ecommerce_db")
+POSTGRES_USER = os.environ.get("POSTGRES_USER", "postgres")
+POSTGRES_HOST = os.environ.get("POSTGRES_HOST", "postgres")
+POSTGRES_PORT = os.environ.get("POSTGRES_PORT", "5432")
+
+def main():
+    spark = SparkSession.builder.appName("HistoricalLoad") \
+        .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
+        .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
+        .getOrCreate()
+    
+    db_properties = {
+        "user": POSTGRES_USER,
+        "password": POSTGRES_PASSWORD,
+        "driver": "org.postgresql.Driver"
+    }
+    jdbc_url = f"jdbc:postgresql://{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DATABASE}"
+    
+    base_path = os.environ.get("DELTALAKE_BASE_PATH")
+    if not base_path:
+        raise ValueError("DELTALAKE_BASE_PATH environment variable not set!")
+
+    tables = ["transacoes_vendas", "eventos_web"]
+
+    print("--- Starting Full Historical Load ---")
+    for table in tables:
+        print(f"Processing table: {table}...")
+        
+        # 1. Read the ENTIRE table from PostgreSQL
+        df = spark.read.jdbc(url=jdbc_url, table=table, properties=db_properties)
+        
+        # 2. OVERWRITE the Delta table with the full dataset
+        delta_path = f"{base_path}/bronze/{table}"
+        print(f"Writing {df.count()} rows to {delta_path} in overwrite mode.")
+        df.write.format("delta").mode("overwrite").option("overwriteSchema", "true").save(delta_path)
+        
+        print(f"Table {table} loaded successfully.")
+
+    print("--- Full Historical Load Complete ---")
+    spark.stop()
+
+if __name__ == "__main__":
+    main()
